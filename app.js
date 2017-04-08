34e9bd95af007b2224fa2153a2227a2b80ee91b9
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-08 12:59:34 -0600

    added slight change to views

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'views/layouts/main'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
