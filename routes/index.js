cee422909a3e69f46c57698c87b127e38e52fd4c
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-04-08 13:51:07 -0600

    created login, error handled

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const router = express.Router();
-const knex = require('../knex')
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('users')
-    .then((users) => {
+   res.render('index');
 
-      res.render('index', { title: 'Express' });
-    })
+  // knex('users')
+  //   .then((users) => {
+  //
+  //   })
 });
+
+router.post('/', (req, res) => {
+   let username = req.body.username
+   let password = req.body.password
+   if (!username || !password){
+      res.render ('index', {error: 'fill it all in you dingus'})
+   } else {
+      knex('users')
+         .where('username', username)
+         .first()
+         .then((user) => {
+            if (!user) {
+               res.render('index', {error: 'you dont exist!!!!! panic.'})
+            } else {
+               console.log('user', user);
+               res.render('index')
+            }
+         })
+   }
+})
 
 
 module.exports = router;
