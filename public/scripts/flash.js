80a9277b8c24a018e49cce7c5fdafd34174a3820
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-09 17:10:17 -0600

    Read functionality is almost there on decks and decks/:id

--- public/scripts/flash.js
+++ public/scripts/flash.js
@@ -3,12 +3,15 @@ $(document).ready(() => {
 
   $('.deck').click(() => {
     console.log('here');
-    let deck_id = $(event.target).attr('id')
-    console.log(deck_id);
+    let deck_id = $(event.target).attr('data-id')
+    console.log('deck_id', deck_id);
     $.ajax({
       method: 'GET',
-      url: '/decks/:id',
-      data: { deck_id: deck_id}
+      url: '',
+      data: { deck_id: deck_id },
+      success: (data) => {
+        location.href = `/decks/${deck_id}`
+      }, error: () => {}
     })
   })
 })
--- routes/decks.js
+++ routes/decks.js
@@ -4,61 +4,40 @@ const bcrypt = require('bcrypt');
 const knex = require('../knex');
 const jwt = require('jsonwebtoken');
 
-
-router.get('/', (req, res, next) => {
-  let userID = req.cookies.userID
-  console.log(userID);
-  knex('users')
-    .join('decks', 'decks.user_id', 'users.id')
-    .select('decks.id as deck_id', 'decks.name as deck_title')
-    .where('users.id', userID)
-    .then((decks) => {
-      res.render('decks', {
-        decks: decks
-      })
-    })
-});
-
 router.get('/:id', (req, res, next) => {
-  console.log('hi there!');
-  let deck_id = req.body.deck_id
-  console.log(deck_id);
+  let deck_id = req.params.id
+  console.log('params', req.params);
+  console.log('query', req.query);
   knex('cards')
     .select('cards.front as front', 'cards.back as back', 'cards.got_it as got_it ')
     .join('decks', 'decks.id', 'cards.deck_id')
     .where('decks.id', deck_id)
     .then((data) => {
-        res.status(200).send(true)
+        res.render(`decks`, {
+          deck: data
+        })
     })
 })
 
-module.exports = router;
+router.get('/', (req, res, next) => {
+  if (req.params.id) {
+    res.status(200).send(true)
+  } else {
+    let userID = req.cookies.userID
+    knex('users')
+      .join('decks', 'decks.user_id', 'users.id')
+      .select('decks.id as deck_id', 'decks.name as deck_title')
+      .where('users.id', userID)
+      .then((decks) => {
+        res.render('decks', {
+          decks: decks
+        })
+      })
+  }
 
-// function deckAndUser (userID) {
-//   return knex('users')
-//     .join('decks', 'decks.user_id', 'users.id')
-//     .select('decks.id as deck_id', 'decks.name as deck_title')
-//     .where('users.id', userID)
-//     .then((decks) => {
-//       return decks
-//     })
-// }
-//
-// function decksAndCards(decks) {
-//   return Promise.all(decks.map((deck) => {
-//     return knex('cards')
-//       .select('cards.front as front', 'cards.back as back', 'cards.got_it as got_it')
-//       .join('decks', 'decks.id', 'cards.deck_id')
-//       .where('decks.id', deck.deck_id)
-//       .then((data) => {
-//         console.log(data);
-//         deck.card = data
-//         return deck
-//       })
-//   }))
-// }
-//
-// function wholeDeck(userID) {
-//   return deckAndUser(userID)
-//     .then(decksAndCards)
-// }
+
+});
+
+
+
+module.exports = router;
