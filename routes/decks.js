b376e2e1b6b71cb4269bb5a9ac97cf4f58c0980c
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-09 15:07:22 -0600

    added to decks

---
+++ routes/decks.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
+
+module.exports = router;
