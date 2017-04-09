24de83efc5653c70f83c83e2588880ec796ed9c1
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-09 14:57:25 -0600

    added decks page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const users = require('./routes/users');
 const signup = require('./routes/signup');
+const decks = require('./routes/decks')
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/signup', signup);
 app.use('/users', users);
+app.use('/decks', decks)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
