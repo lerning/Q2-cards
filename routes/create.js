f1d8474d0d3858fee013e0e2aa5c9f4a295fee1f
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-10 15:17:22 -0600

    added some jwt.verify to decks an create page

--- routes/create.js
+++ routes/create.js
@@ -5,28 +5,34 @@ const knex = require('../knex');
 const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
-   res.render('create')
+  jwt.verify(req.cookies.token, 'shhhh', (err, decoded) => {
+    if (decoded) {
+      res.render('create')
+    } else {
+      res.redirect('/?unauthorized=true')
+    }
+  })
 })
 
 router.post('/', (req, res) => {
-   console.log('body', req.body);
-   console.log(' front reqbody [1]', req.body.front[1]);
-   console.log(' back reqbody [1]', req.body.back[1]);
-   //knex to add deck
+  console.log('body', req.body);
+  console.log(' front reqbody [1]', req.body.front[1]);
+  console.log(' back reqbody [1]', req.body.back[1]);
+  //knex to add deck
 
 
 
 
-   //knex to add card
-   for (let card in req.body.front){
-      console.log(req.body.front[card]);
-      console.log(req.body.back[card]);
-   }
-   // let card  = {req.body.front[1]: req.body.back[1]}
-   // console.log('card obj', card);
-   // knex('cards')
+  //knex to add card
+  for (let card in req.body.front) {
+    console.log(req.body.front[card]);
+    console.log(req.body.back[card]);
+  }
+  // let card  = {req.body.front[1]: req.body.back[1]}
+  // console.log('card obj', card);
+  // knex('cards')
 
-   res.render('create')
+  res.render('create')
 
 })
 
--- routes/decks.js
+++ routes/decks.js
@@ -4,35 +4,47 @@ const bcrypt = require('bcrypt');
 const knex = require('../knex');
 const jwt = require('jsonwebtoken');
 
-router.get('/:id', (req, res, next) => {
-  let deck_id = req.params.id
-  knex('cards')
-    .select('cards.front as front', 'cards.back as back', 'cards.got_it as got_it ')
-    .join('decks', 'decks.id', 'cards.deck_id')
-    .where('decks.id', deck_id)
-    .then((data) => {
-        res.render(`decks`, {
-          deck: data
-        })
-    })
-})
 
 router.get('/', (req, res, next) => {
-  if (req.params.id) {
-    res.status(200).send(true)
-  } else {
-    let userID = req.cookies.userID
-    knex('users')
-      .join('decks', 'decks.user_id', 'users.id')
-      .select('decks.id as deck_id', 'decks.name as deck_title')
-      .where('users.id', userID)
-      .then((decks) => {
-        res.render('decks', {
-          decks: decks
-        })
-      })
-  }
+    jwt.verify(req.cookies.token, 'shhhh', (err, decoded) => {
+      if (decoded) {
+        if (req.params.id) {
+          res.status(200).send(true)
+        } else {
+          let userID = req.cookies.userID
+          knex('users')
+          .join('decks', 'decks.user_id', 'users.id')
+          .select('decks.id as deck_id', 'decks.name as deck_title')
+          .where('users.id', userID)
+          .then((decks) => {
+            res.render('decks', {
+              decks: decks
+            })
+          })
+      }
+    } else {
+      res.redirect('/?unauthorized=true')
+    }
+  })
 
+  router.get('/:id', (req, res, next) => {
+    jwt.verify(req.cookies.token, 'secret_key', (err, decoded) => {
+      if (decoded) {
+        let deck_id = req.params.id
+        knex('cards')
+        .select('cards.front as front', 'cards.back as back', 'cards.got_it as got_it ')
+        .join('decks', 'decks.id', 'cards.deck_id')
+        .where('decks.id', deck_id)
+        .then((data) => {
+          res.render(`decks`, {
+            deck: data
+          })
+        })
+      } else {
+        res.redirect('/?unauthorized=true')
+      }
+    })
+  })
 
 });
 
