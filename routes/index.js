cbe6ff12594537060dd7951796d43f6c74b9f4f1
Emily Ringoen <emilyaringoen@gmail.com>
2017-04-10 14:35:59 -0600

    clear cookies...check

--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,48 @@ const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   res.render('index');
-
-  // knex('users')
-  //   .then((users) => {
-  //
-  //   })
+  res.clearCookie('token')
+  res.clearCookie('userID')
+  res.render('index');
 });
 
 router.post('/', (req, res) => {
-   let username = req.body.username.toLowerCase()
-   let password = req.body.password
-   if (!username || !password){
-      res.render ('index', {error: 'fill it all in you dingus'})
-   } else {
-      knex('users')
-         .where('username', username)
-         .first()
-         .then((user) => {
-            if (!user) {
-               res.render('index', {error: 'you dont exist!!!!! panic.'})
+  let username = req.body.username.toLowerCase()
+  let password = req.body.password
+  if (!username || !password) {
+    res.render('index', {
+      error: 'fill it all in you dingus'
+    })
+  } else {
+    knex('users')
+      .where('username', username)
+      .first()
+      .then((user) => {
+        if (!user) {
+          res.render('index', {
+            error: 'you dont exist!!!!! panic.'
+          })
+        } else {
+          let hashed_password = bcrypt.compare(password, user.hashed_password, (err, result) => {
+            if (result) {
+              let token = jwt.sign({
+                user: user
+              }, 'secret_key')
+              res.cookie('token', token)
+              res.cookie('userID', user.id, {
+                httpOnly: true
+              })
+              res.redirect('/decks')
             } else {
-               let hashed_password = bcrypt.compare(password, user.hashed_password, (err, result) => {
-                  if (result) {
-                     let token = jwt.sign({user: user}, 'secret_key')
-                     res.cookie('token', token)
-                     res.cookie('userID', user.id, { httpOnly:true })
-                     res.redirect('/decks')
-                  }else {
-                    console.log('else');
-                     res.render('index', {error: 'incorrect password you jabrone'})
-                  }
-               })
+              console.log('else');
+              res.render('index', {
+                error: 'incorrect password you jabrone'
+              })
             }
-         })
-   }
+          })
+        }
+      })
+  }
 })
 
 
