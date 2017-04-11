162598ffe3be932e274644aaa7b105af8574bd3c
rhysgoehring <rhysgoehring@gmail.com>
2017-04-11 12:45:33 -0600

    preparing for deployment

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,9 @@ module.exports = {
   development: {
      client: 'pg',
      connection: 'postgres://localhost/flash_db',
- }
+ },
+  producton: {
+    "client": "pg",
+    "connection": process.env.DATABASE_URL
+  }
 };
